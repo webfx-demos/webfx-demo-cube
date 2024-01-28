b0d7ecbbf2bb2ac579aba8da8dd717d833205e15
Bruno Salmon <dev.salmonb@gmail.com>
2024-01-28 15:29:27 +0000

    Renamed WebGL demo to Cube demo

---
+++ webfx-demo-cube-application/src/main/java/module-info.java
@@ -0,0 +1,36 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.cube.application {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires webfx.extras.util.color;
+    requires webfx.kit.launcher;
+    requires webfx.kit.util;
+    requires webfx.kit.webgl;
+    requires webfx.lib.joml;
+    requires webfx.platform.console;
+    requires webfx.platform.resource;
+    requires webfx.platform.typedarray;
+    requires webfx.platform.uischeduler;
+
+    // Exported packages
+    exports dev.webfx.demo.cube;
+    exports dev.webfx.demo.cube.cubescene;
+    exports dev.webfx.demo.cube.smokescene;
+    exports dev.webfx.demo.cube.textscene;
+    exports dev.webfx.demo.cube.textscene.interpol;
+
+    // Resources packages
+    opens dev.webfx.demo.cube;
+    opens dev.webfx.demo.cube.cubescene;
+    opens dev.webfx.demo.cube.smokescene;
+    opens dev.webfx.demo.cube.textscene;
+
+    // Provided services
+    provides javafx.application.Application with dev.webfx.demo.cube.CubeApplication;
+
+}
