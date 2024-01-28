2fdb5adec3fe99ed2e09c8fac2280af89eacdbab
Bruno Salmon <dev.salmonb@gmail.com>
2024-01-28 15:35:29 +0000

    Renamed WebGL demo to Cube demo (2)

---
+++ webfx-demo-cube-application-gluon/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.cube.application.gluon {
+
+    // Direct dependencies modules
+    requires javafx.media;
+    requires webfx.demo.cube.application;
+    requires webfx.kit.openjfx;
+    requires webfx.platform.audio.gluon;
+    requires webfx.platform.boot.java;
+    requires webfx.platform.console.java;
+    requires webfx.platform.os.gluon;
+    requires webfx.platform.resource.gluon;
+    requires webfx.platform.scheduler.java;
+    requires webfx.platform.shutdown.gluon;
+
+}
---
+++ webfx-demo-cube-application-openjfx/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.cube.application.openjfx {
+
+    // Direct dependencies modules
+    requires javafx.media;
+    requires webfx.demo.cube.application;
+    requires webfx.kit.openjfx;
+    requires webfx.platform.boot.java;
+    requires webfx.platform.console.java;
+    requires webfx.platform.os.java;
+    requires webfx.platform.resource.java;
+    requires webfx.platform.scheduler.java;
+    requires webfx.platform.shutdown.java;
+
+}
